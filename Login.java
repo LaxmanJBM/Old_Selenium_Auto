815292c92b5280028b9a4c5c5fbaaa79c60fd0cd
LaxmanJBM <124760792+LaxmanJBM@users.noreply.github.com>
2023-03-07 17:57:12 +0530

    Add files via upload
    Small Change

---
+++ Login.java
@@ -0,0 +1,22 @@
+package Browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import Utility.Files;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Login extends Files{
+	
+protected static WebDriver driver;
+	
+	public void initilizationB()
+	{
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();                                         //We can take test data in same page under the Network MemberType Profile
+	//	driver.get(readExcelFile(0, 0));
+		driver.get("https://jbmahuat.jbmcloud.com/JBM_HIMS/General/Forms/LoginV2.aspx");
+		driver.manage().window().maximize();
+	}
+}
